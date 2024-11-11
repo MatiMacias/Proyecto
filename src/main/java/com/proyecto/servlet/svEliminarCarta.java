6c632f20437da10f99a02b3146dc8986a40080fb
Santiago González <santigonzalez1224@gmail.com>
2024-11-11 16:08:38 -0300

    Modificacion de Carta

--- src/main/java/com/proyecto/servlet/svEliminarCarta.java
+++ src/main/java/com/proyecto/servlet/svEliminarCarta.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+    
 package com.proyecto.servlet;
 
 import com.proyecto.logica.ControladoraLogica;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,13 +11,14 @@ import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Matias
+ * @author Santi
  */
+
 @WebServlet(name = "svEliminarCarta", urlPatterns = {"/svEliminarCarta"})
 public class svEliminarCarta extends HttpServlet {
-
-    ControladoraLogica ctrl = new ControladoraLogica();
     
+    ControladoraLogica ctrl = new ControladoraLogica();
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
     }
@@ -40,12 +37,12 @@ public class svEliminarCarta extends HttpServlet {
         int idBorrar = Integer.parseInt(request.getParameter("idCarta"));
         
         ctrl.borrarCarta(idBorrar);
-        
+      
+        response.sendRedirect("index.jsp");
     }
 
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
-
+    }
 }
